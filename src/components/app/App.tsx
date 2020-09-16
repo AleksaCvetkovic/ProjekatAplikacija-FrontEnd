0eb0437e3659ac88d0d5bf034d1f8167bbffeb52
Aleksa Cvetkovic <aleksacvele3@gmail.com>
2020-09-17 01:05:20 +0200

    main manu component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,10 +4,11 @@ import {Card, Container} from 'react-bootstrap';
 import { faHome, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+
 function App() {
   return (
     <Container>
-
+     
       <FontAwesomeIcon icon = { faHome } /> Home
     </Container>
   );
