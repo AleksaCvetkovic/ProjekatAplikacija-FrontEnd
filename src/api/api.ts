80ee2cc442916f15bb08bd35805dec35320a78e1
Aleksa Cvetkovic <aleksacvele3@gmail.com>
2020-09-17 16:42:46 +0200

    Logovanje usera front end

---
+++ src/api/api.ts
@@ -0,0 +1,151 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { ApiConfig } from '../config/apiConfig';
+
+export default function api(
+    path: string, 
+    method: 'get' | 'post' | 'patch' | 'delete',
+    body: any | undefined,
+){
+    return new Promise<ApiResponse>((resolve) => {
+        const requestData = {
+            
+                method: method,
+                url: path,
+                baseURL: ApiConfig.API_URL,
+                data: JSON.stringify(body),
+                headers: {
+                    'Content-Type': 'aplication/json',
+                    'Authorization': getToken(),
+       
+                },
+        }
+     axios(requestData)
+     .then(res => responseHendler( res,resolve, requestData ))
+     .catch(err => {
+                const response: ApiResponse = {
+                    status: 'error',
+                    data: err
+                };
+            resolve(response);
+         });
+    });
+}
+
+export interface ApiResponse {
+    status: 'ok' | 'error' | 'login';
+    data: any;
+}
+
+async function responseHendler(
+    res: AxiosResponse<any>,
+    resolve: (value?: ApiResponse) => void,
+    requestData: AxiosRequestConfig,
+    ) {
+        if (res.status < 200 || res.status >= 300){
+
+            if(res.status === 401){
+                const newToken = await refreshToken(requestData);
+
+                if(!newToken){
+                    const response: ApiResponse = {
+                        status: 'login',
+                        data: null,
+                    };
+                    return resolve(response);
+                }
+                saveToken(newToken);
+
+                requestData.headers['Authorization'] = getToken();
+
+               return await repeatRequest(requestData, resolve);
+            }
+
+            const response: ApiResponse = {
+                status: 'error',
+                data: res.data,
+            }
+
+            return resolve(response);
+        }
+
+        if(res.data.statusCode < 0){
+
+            const response: ApiResponse = {
+                status: 'ok',
+                data: res.data,
+            };
+            return resolve(response);
+        }
+
+        resolve(res.data);
+}
+function getToken(): string {
+    const token = localStorage.getItem('api_token');
+
+    return 'Berer ' + token;
+}
+
+export function saveToken(token: string){
+    localStorage.setItem('api_token', token);
+}
+function getRefreshToken(): string{
+    const token = localStorage.getItem('api_refresh_token');
+    return  token + '';
+}
+export function saveRefreshToken(token: string){
+    localStorage.setItem('api_refresh_token', token);
+}
+
+async function refreshToken(
+    requestData: AxiosRequestConfig, 
+    ): Promise<string | null> {
+        const path = 'user/refresh';
+        const data = {
+            token: getRefreshToken(),
+        }
+
+        const refreshTokenRequestData: AxiosRequestConfig = {
+            
+            method: 'post',
+            url: path,
+            baseURL: ApiConfig.API_URL,
+            data: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'aplication/json',
+   
+            },
+    };
+  const rtr: {data: {token: string | undefined} } = await axios(refreshTokenRequestData);
+  if (!rtr.data.token){
+      return null;
+  }
+     return rtr.data.token;
+}
+async function repeatRequest(
+    requestData: AxiosRequestConfig, 
+    resolve: (value?: ApiResponse) => void){
+        axios(requestData)
+        .then(res => {
+            let response: ApiResponse;
+            if(res.status === 401) {
+                 response = {
+                    status: 'login',
+                    data: null,
+                };
+            }else {
+                 response = {
+                    status: 'ok',
+                    data: res,
+                };
+            }
+                return resolve(response);
+        })
+        .catch(res =>{
+            const response: ApiResponse = {
+                status: 'error',
+                data: res.data,
+            }
+
+            return resolve(response);
+        });
+}
